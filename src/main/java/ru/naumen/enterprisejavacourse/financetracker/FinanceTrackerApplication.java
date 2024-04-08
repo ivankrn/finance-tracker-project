73551af541adaba25db49d6b88dd9ba37a217b9b
Ivan Kropotin <kropotiniv@yandex.ru>
2024-04-08 13:47:02 +0500

    Initial commit

---
+++ src/main/java/ru/naumen/enterprisejavacourse/financetracker/FinanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package ru.naumen.enterprisejavacourse.financetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerApplication.class, args);
+	}
+
+}
