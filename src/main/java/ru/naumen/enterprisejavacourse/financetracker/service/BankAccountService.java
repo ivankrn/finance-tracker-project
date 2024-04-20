1f727159fae0ed2d10bd5d75d4c0f041db582d9e
Ivan Kropotin <kropotiniv@yandex.ru>
2024-04-20 15:20:23 +0500

    Добавил javadoc'и

--- src/main/java/ru/naumen/enterprisejavacourse/financetracker/service/BankAccountService.java
+++ src/main/java/ru/naumen/enterprisejavacourse/financetracker/service/BankAccountService.java
@@ -2,9 +2,24 @@ package ru.naumen.enterprisejavacourse.financetracker.service;
 
 import ru.naumen.enterprisejavacourse.financetracker.database.model.User;
 
+/**
+ * Сервис для управления банковскими счетами
+ */
 public interface BankAccountService {
 
+    /**
+     * Создает счет для пользователя
+     *
+     * @param user пользователь
+     * @param currencyCode валюта счета
+     */
     void addForUser(User user, String currencyCode);
+
+    /**
+     * Удаляет счет с указанным ID
+     *
+     * @param accountId ID счета
+     */
     void delete(long accountId);
 
 }
