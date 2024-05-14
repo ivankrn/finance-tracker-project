b52cef0beec17ddf658fa02ba44fb9fc20d8350f
mikhailemv <emelianovmkevg@yandex.ru>
2024-05-14 14:55:38 +0500

    Исправил замечания.

--- src/main/java/ru/naumen/enterprisejavacourse/financetracker/mapper/TransactionMapper.java
+++ src/main/java/ru/naumen/enterprisejavacourse/financetracker/mapper/TransactionMapper.java
@@ -2,15 +2,14 @@ package ru.naumen.enterprisejavacourse.financetracker.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mappings;
 import ru.naumen.enterprisejavacourse.financetracker.database.model.Transaction;
 import ru.naumen.enterprisejavacourse.financetracker.dto.TransactionDto;
 
 /**
  * Интерфейс маппера для конвертации сущностей Transaction в объекты TransactionDto
  */
-@Mapper
-@Component
+@Mapper(componentModel = "spring")
 public interface TransactionMapper {
 
     /**
@@ -19,7 +18,9 @@ public interface TransactionMapper {
      * @param transaction сущность Transaction
      * @return TransactionDto на основе сущности Transaction
      */
-    @Mapping(source = "category.id", target = "categoryId")
-    @Mapping(source = "bankAccount.id", target = "bankAccountId")
+    @Mappings({
+            @Mapping(source = "category.id", target = "categoryId"),
+            @Mapping(source = "bankAccount.id", target = "bankAccountId")
+    })
     TransactionDto transactionToTransactionDto(Transaction transaction);
 }
